747b569d7a2dee3a4eff84c1acce94433a717281
jongchan lee <95720988+bistwars00@users.noreply.github.com>
2022-05-03 04:34:24 +0900

    Update DiaryList.js

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -33,12 +33,11 @@ const ControlMenu = React.memo(({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const navigate = useNavigate();
-  const [sortType, setSortType] = useState("latest"); // 최근 생성 순서 정렬 데이터 저장 state
-  const [filter, setFilterType] = useState("all"); // 감정 필터를 저장할 state
+  const [sortType, setSortType] = useState("latest"); 
+  const [filter, setFilterType] = useState("all"); 
 
   const getProcessedDiaryList = () => {
     const filterCallBack = (item) => {
-      // 좋음과 나쁨의 상태를 구분하기위한 함수
       if (filter === "good") {
         return parseInt(item.emotion) <= 3;
       } else {
@@ -54,10 +53,10 @@ const DiaryList = ({ diaryList }) => {
       }
     };
 
-    const copyList = JSON.parse(JSON.stringify(diaryList)); //diaryList 원본을 놔두고, 복사해서 정렬 기능 사용하기위해, 원본에다 sort쓰면 오류남
+    const copyList = JSON.parse(JSON.stringify(diaryList)); 
 
     const filteredList =
-      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it)); //위에 좋음과 나쁨의 상태 구분위한 함수를 적용해줌
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it)); 
 
     const sortedList = filteredList.sort(compare);
     return sortedList;
@@ -82,7 +81,7 @@ const DiaryList = ({ diaryList }) => {
           <MyButton
             type={"positive"}
             text={"새 일기쓰기"}
-            onClick={() => navigate("/new")} // 페이지 이동 태그
+            onClick={() => navigate("/new")} 
           />
         </div>
       </div>
