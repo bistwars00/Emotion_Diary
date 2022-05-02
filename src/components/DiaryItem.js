b3199d0c9d80fdbf7c6568695eaf0bb9ccd54d5e
jongchan lee <95720988+bistwars00@users.noreply.github.com>
2022-05-03 04:33:42 +0900

    Update DiaryItem.js

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -4,18 +4,18 @@ import MyButton from "./MyButton";
 import { useNavigate } from "react-router-dom";
 
 const DiaryItem = ({ id, emotion, content, date }) => {
-  const navigate = useNavigate(); //페이지 이동시킬 변수
+  const navigate = useNavigate(); 
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-  const strDate = new Date(parseInt(date)).toLocaleDateString(); //date를 yyyy-mm-dd형식으로 바꿔줌
+  const strDate = new Date(parseInt(date)).toLocaleDateString(); 
 
   const goDetail = () => {
-    navigate(`/diary/${id}`); // 각 일기의 번호에 따라서, 상세 페이지로 이동시킴
+    navigate(`/diary/${id}`); 
   };
 
   const goEdit = () => {
-    navigate(`/edit/${id}`); // 각 일기의 번호에 따라서, 수정 페이지로 이동시킴
+    navigate(`/edit/${id}`); 
   };
 
   return (
@@ -25,7 +25,7 @@ const DiaryItem = ({ id, emotion, content, date }) => {
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
-        ].join(" ")} // 쉼표 없애려고 join 씀, 동적으로 클래스 네임 만들어줌
+        ].join(" ")} 
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
