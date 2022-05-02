147a04fd50047ecf4ab99c7348a83e402aba3648
jongchan lee <95720988+bistwars00@users.noreply.github.com>
2022-05-03 04:34:48 +0900

    Update EmotionItem.js

--- src/components/EmotionItem.js
+++ src/components/EmotionItem.js
@@ -7,14 +7,13 @@ const EmotionItem = ({
   onClick,
   isSelected,
 }) => {
-  // 그림 클릭시, 어떤 그림 선택했는지, state 반영해줌
   return (
     <div
       onClick={() => onClick(emotion_id)}
       className={[
         "EmotionItem",
         isSelected ? `EmotionItem_on_${emotion_id}` : `EmotionItem_off`,
-      ].join(" ")} //선택여부에 따라 on/off 부여해서, 백그라운드 색 지정 가능
+      ].join(" ")} 
     >
       <img src={emotion_img} />
       <span>{emotion_descript}</span>
